f069349791d11219511d10529b093152c4c3be35
Sergio Garcia <undostres321@gmail.com>
2019-10-29 17:25:13 +0100

    Exercise 1.7 solution

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -5,6 +5,28 @@ const Button = ({onClick, text}) => (
   <button onClick={ onClick }>{ text }</button>
 )
 
+const Average = ({good, bad, neutral}) => {
+  if ((good === 0) && (neutral === 0) && (bad === 0)) {
+    return (
+      <div>average 0</div>
+    )
+  }
+  return (
+    <div>average { (good - bad) / (good + neutral + bad) }</div>
+  )
+}
+
+const PositivePercentage = ({good, bad, neutral}) => {
+  if ((good === 0) && (neutral === 0) && (bad === 0)) {
+    return (
+      <div>positive 0</div>
+    )
+  }
+  return (
+    <div>positive {good * 100 / (good + neutral + bad)} %</div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
@@ -28,16 +50,21 @@ const App = () => {
     }
   }
 
+  
+
   return (
     <div>
       <h1>give feedback</h1>
-      <Button onClick = { handlerClick('good') } text = 'Good'></Button>
-      <Button onClick = { handlerClick('neutral') } text = 'Neutral'></Button>
-      <Button onClick = { handlerClick('bad') } text = 'Bad'></Button>
+      <Button onClick = { handlerClick('good') } text='Good'></Button>
+      <Button onClick = { handlerClick('neutral') } text='Neutral'></Button>
+      <Button onClick = { handlerClick('bad') } text='Bad'></Button>
       <h1>statistics</h1>
       <div>good { good }</div>
       <div>neutral { neutral }</div>
       <div>bad { bad }</div>
+      <div>all { good + neutral + bad }</div>
+      <Average good={ good } bad={ bad } neutral={ neutral }></Average>
+      <PositivePercentage good={ good } bad={ bad } neutral={ neutral }></PositivePercentage>
     </div>
   )
 }
